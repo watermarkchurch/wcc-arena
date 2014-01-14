d2ea4f1c83d5c13a97b4fc2fbd044aeb4cdba8fe
Travis Petticrew <travis@petticrew.net>
2014-01-14 12:10:48 -0600

    Added address association on Group

--- lib/wcc/arena/group.rb
+++ lib/wcc/arena/group.rb
@@ -1,5 +1,7 @@
 require 'wcc/arena/group_query'
 
+require 'wcc/arena/address'
+
 module WCC::Arena
   class Group
     include WCC::Arena::Mappers::XML
@@ -13,5 +15,7 @@ module WCC::Arena
     attribute :type, xpath: "GroupTypeValue"
     attribute :type_id, xpath: "GroupTypeID", type: :integer
     attribute :leader_id, xpath: "LeaderID", type: :integer
+
+    has_one :address, xpath: "TargetLocation", klass: Address
   end
 end
